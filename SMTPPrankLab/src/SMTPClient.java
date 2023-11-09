61c2d242994d7d7dd52c33bffeebae88d3028444
Simeline <valentin.bugna@heig-vd.ch>
2023-11-09 22:10:34 +0100

    png and docker command

--- SMTPPrankLab/src/SMTPClient.java
+++ SMTPPrankLab/src/SMTPClient.java
@@ -5,16 +5,21 @@ public class SMTPClient {
     public static void main(String[] args) {
 
         try {
-            Socket socket = new Socket("localhost", 25);
+            // il faut exécuter dans le terminal la commande : docker run -d -p 1080:1080 -p 1025:1025 maildev/maildev
+            Socket socket = new Socket("localhost", 1025);
             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+
+            // Lire la réponse
+            String response = in.readLine();
+            System.out.println("Réponse du serveur : " + response + "\n");
 
             // Étape 1 : Se connecter au serveur SMTP
             out.println("HELO localhost");
 
             // Lire la réponse
-            String response = in.readLine();
-            System.out.println("Réponse du serveur : " + response);
+            response = in.readLine();
+            System.out.println("Réponse du serveur : " + response + "\n");
 
             // Étape 2 : Envoyer l'e-mail
             // ...
@@ -23,6 +28,7 @@ public class SMTPClient {
             in.close();
             out.close();
             socket.close();
+            
         } catch (IOException e) {
             e.printStackTrace();
         }

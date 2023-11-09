cc4f76bc9beda90121904886197a69c9cd956e73
Simeline <valentin.bugna@heig-vd.ch>
2023-11-09 16:39:24 +0100

    json files added

--- SMTPPrankLab/src/LectureVictimes.java
+++ SMTPPrankLab/src/LectureVictimes.java
@@ -3,11 +3,40 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class LectureVictimes {
+
+    public static void main(String[] args) {
+        String pathVictimes = "config/victims.txt";
+        List<String> victimes = lireListeVictimes(pathVictimes);
+
+        // Afficher les victimes
+        for (String victime : victimes) {
+            System.out.println(victime);
+        }
+
+        String pathMessagesJ = "config/messagesJ.json"; // Assurez-vous que le chemin soit correct
+        List<Message> messagesJ = lireListeMessagesJSON(pathMessagesJ);
+
+        // Afficher les messages
+        for (int i = 0; i < messagesJ.size(); i++) {
+            System.out.println((i + 1) + ")" + messagesJ.get(i));
+        }
+
+        String pathMessages = "config/messages.txt";
+        List<String> messages = lireListeMessagesTXT(pathMessages);
+
+//        // Afficher les messages
+//        for (int i = 0; i < messages.size(); i++) {
+//            System.out.println((i + 1) + ")" + messages.get(i));
+//        }
+    }
 
     public static final Pattern ADDRESSE_MAIL_VALIDE =
             Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
@@ -18,7 +47,7 @@ public class LectureVictimes {
     }
 
     // static parce que c'est indépendant de la classe, on peut utiliser la fonction sans créer / instancier la classe
-    public static List<String> lireListeMessages(String path) {
+    public static List<String> lireListeMessagesTXT(String path) {
 
         List<String> messages = new ArrayList<>();
         StringBuilder messageCourant = new StringBuilder();
@@ -46,6 +75,34 @@ public class LectureVictimes {
         return messages;
     }
 
+    public static List<Message> lireListeMessagesJSON(String path) {
+        List<Message> messages = new ArrayList<>();
+
+        try (var reader = new BufferedReader(new FileReader(path))) {
+            StringBuilder jsonContent = new StringBuilder();
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                jsonContent.append(line);
+            }
+
+            JSONArray jsonArray = new JSONArray(jsonContent.toString());
+
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                String subject = jsonObject.getString("Subject");
+                String body = jsonObject.getString("Body");
+
+                Message message = new Message(subject, body);
+                messages.add(message);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return messages;
+    }
+
     public static List<String> lireListeVictimes(String path) {
 
         List<String> victimes = new ArrayList<>();
@@ -65,23 +122,5 @@ public class LectureVictimes {
         }
 
         return victimes;
-    }
-
-    public static void main(String[] args) {
-        String pathVictimes = "config/victims.txt";
-        List<String> victimes = lireListeVictimes(pathVictimes);
-
-        // Afficher les victimes
-        for (String victime : victimes) {
-            System.out.println(victime);
-        }
-
-        String pathMessages = "config/messages.txt";
-        List<String> messages = lireListeMessages(pathMessages);
-
-//        // Afficher les messages
-//        for (int i = 0; i < messages.size(); i++) {
-//            System.out.println((i + 1) + ")" + messages.get(i));
-//        }
     }
 }
---
+++ SMTPPrankLab/src/Message.java
@@ -0,0 +1,22 @@
+public class Message {
+    private String subject;
+    private String body;
+
+    public Message(String subject, String body) {
+        this.subject = subject;
+        this.body = body;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    @Override
+    public String toString() {
+        return "Subject: " + subject + "\n" + body;
+    }
+}

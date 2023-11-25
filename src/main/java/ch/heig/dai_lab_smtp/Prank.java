83b2b12be3315f5c0653c138ff957a3027ca0af7
Vicky <56110187+Kalysso@users.noreply.github.com>
2023-11-25 14:34:47 +0100

    Improve Prank class

--- src/main/java/ch/heig/dai_lab_smtp/Prank.java
+++ src/main/java/ch/heig/dai_lab_smtp/Prank.java
@@ -1,35 +1,61 @@
 package ch.heig.dai_lab_smtp;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Prank {
-    private final String sender;
-    private final Group receivers;
+    private final String fileVictims;
+    private final String fileMessages;
 
-    /**
-     * Constructeur qui permet de séparer l'émetteur des récepteurs
-     */
-    public Prank() {
-        this(5);
+    private final int numberPersonPerGroup;
+    private String sender;
+    private List<String> receivers = new ArrayList<>();
+    private ReadFiles.Message message;
+    private String subject;
+
+    public Prank(String fileVictims, String fileMessages) {
+        this(fileVictims, fileMessages, 5);
     }
 
-    public Prank(int groupSize) {
-        this.receivers = new Group(groupSize);
-        this.sender = receivers.getListeVictimes().get(0);
-        receivers.getListeVictimes().remove(0);
+    public Prank(String fileVictims, String fileMessages, int groupSize) {
+        this.fileVictims = fileVictims;
+        this.fileMessages = fileMessages;
+        this.numberPersonPerGroup = groupSize;
     }
 
-    /**
-     * Affichage des deux catégories d'un groupe
-     */
-    public void display() {
-        StringBuilder messageCourant = new StringBuilder();
-        messageCourant.append("****************** I'm the sender : ************************\n").append(sender).append("\n");
-        messageCourant.append("****************** here is the list of my victims : ********\n");
-        for (int i = 0; i < receivers.sizeOfVictims(); i++) {
-            messageCourant.append(receivers.getListeVictimes().get(i)).append("\n");
+    public String getSender(){
+        return this.sender;
+    }
+
+    public List<String> getReceivers(){
+        return this.receivers;
+    }
+
+    public String getMessageSubject(){
+        return this.message.getSubject();
+    }
+    public String getMessageBody(){
+        return this.message.getBody();
+    }
+    public ReadFiles.Message getMessage(){
+        return this.message;
+    }
+
+    public void generateNewPrank(){ // TODO : Improve by storing full list so we don't need to read file multiple times ?
+        // Clear the receivers list
+        this.receivers.clear();
+
+        // Get the sender and receivers
+        List<String> victimes = ReadFiles.lireListeVictimes(fileVictims, true);
+        int i;
+        for (i = 0; i < this.numberPersonPerGroup -1; i++) {
+            this.receivers.add(victimes.get(i));
         }
-        System.out.println(messageCourant);
+        this.sender = victimes.get(i);
+
+        // Get the message
+        List<ReadFiles.Message> messages = ReadFiles.lireListeMessagesJSON(fileMessages, true);
+        this.message = messages.getFirst();
     }
-
-
-
 }

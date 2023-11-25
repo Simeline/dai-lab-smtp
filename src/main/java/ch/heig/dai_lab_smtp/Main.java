9fd3f36de135ac28b729fff4c2bd44d3e43811b2
Vicky <56110187+Kalysso@users.noreply.github.com>
2023-11-25 14:35:29 +0100

    Implement main

--- src/main/java/ch/heig/dai_lab_smtp/Main.java
+++ src/main/java/ch/heig/dai_lab_smtp/Main.java
@@ -3,42 +3,21 @@ package ch.heig.dai_lab_smtp;
 import java.util.List;
 
 public class Main {
+    final static String FILE_VICTIMES = "src/main/resources/victims.txt";
+    final static String FILE_MESSAGES = "src/main/resources/messagesJ.json";
 
     public static void main(String[] args) {
 
-        String pathVictimes = "src/main/resources/victims.txt";
-        List<String> victimes = ReadFiles.lireListeVictimes(pathVictimes);
+        // TODO : Traitement des arguments et remplacer les variables ci-dessous + constantes de fichiers
+        int nbGroups = 2;
+        int nbPersonPerGroups = 3;
 
-//        // Afficher les victimes
-//        for (String victime : victimes) {
-//            System.out.println(victime);
-//        }
+        SMTPClient client = new SMTPClient();
+        Prank prank = new Prank(FILE_VICTIMES, FILE_MESSAGES, nbPersonPerGroups);
 
-        String pathMessagesJ = "src/main/resources/messagesJ.json"; // Assurez-vous que le chemin soit correct
-        List<ReadFiles.Message> messagesJ = ReadFiles.lireListeMessagesJSON(pathMessagesJ);
-
-//        // Afficher les messages du file json
-//        for (int i = 0; i < messagesJ.size(); i++) {
-//            System.out.println((i + 1) + ")\n" + messagesJ.get(i) + "\n");
-//        }
-
-        String pathMessages = "src/main/resources/messages.txt";
-        List<String> messages = ReadFiles.lireListeMessagesTXT(pathMessages);
-
-//        // Afficher les messages du file txt
-//        for (int i = 0; i < messages.size(); i++) {
-//            System.out.println((i + 1) + ")" + messages.get(i));
-//        }
-
-        Group newGrp = new Group();
-
-//        // Afficher par défaut 3 adresses mail tirées aléatoirement du fichier victims.txt.
-//        for (Mail m : newGrp.getListeVictimes())
-//            System.out.println(m.getMail());
-
-        Prank p = new Prank();
-
-        // Afficher la répartition du groupe en vue d'envoyer les mails et de l'implémentation de fonction
-        p.display();
+        for (int i = 0; i < nbGroups; i++) {
+            prank.generateNewPrank();
+            client.run(prank.getMessageSubject(), prank.getMessageBody(), prank.getSender(), prank.getReceivers());
+        }
     }
 }
